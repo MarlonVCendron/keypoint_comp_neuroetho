3e96b3095393c7ea5af4b5bb586b924766ec7963
Marlon <marlonvcendron@gmail.com>
2025-05-24 13:30:00 -0300

    load data

--- utils/load_data_and_config.py
+++ utils/load_data_and_config.py
@@ -2,14 +2,22 @@ import keypoint_moseq as kpms
 from utils.print_legal import print_legal
 
 
+def load_config(project_dir_path):
+    """Carrega a configuração do projeto."""
+    return kpms.load_config(project_dir_path)
+
+def load_keypoints(project_dir_path):
+    """Carrega os keypoints do DLC."""
+    config = load_config(project_dir_path)
+    coordinates, confidences, bodyparts = kpms.load_keypoints(
+        config["video_dir"], "deeplabcut", extension=".csv"
+    )
+    return coordinates, confidences, bodyparts
+
 def load_data_and_config(project_dir_path):
     """Carrega a configuração e os keypoints do DLC."""
     print_legal(f"Carregando configuração e keypoints do DLC de: {project_dir_path}")
-    config = kpms.load_config(project_dir_path)
-    coordinates, confidences, bodyparts = kpms.load_keypoints(
-        config["video_dir"],
-        "deeplabcut",
-        extension=".csv",
-    )
+    config = load_config(project_dir_path)
+    coordinates, confidences = load_keypoints(project_dir_path)
     data, metadata = kpms.format_data(coordinates, confidences, **config)
     return data, metadata, config
