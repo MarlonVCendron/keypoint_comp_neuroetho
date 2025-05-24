9a8e408ce8bff520af42869111cb92864cd54ed2
Marlon <marlonvcendron@gmail.com>
2025-05-24 09:08:20 -0300

    args

---
+++ utils/args.py
@@ -0,0 +1,37 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="Keypoint MoSeq. Requer um diretório de projeto com arquivo de configuração do DeepLabCut (ex: `config.yml`) dentro dele.")
+parser.add_argument("--project_dir", type=str, help="Diretório do projeto (deve conter config.yml).")
+parser.add_argument("--model_name", type=str, default="teste", help="Nome base para o modelo.")
+parser.add_argument("--mixed-map-iters", type=int, default=8, help="Número de batches. Diminui o uso de memória em X vezes, aumenta o tempo de execução em X vezes")
+
+subparsers = parser.add_subparsers(
+  dest="command",
+  required=True, 
+  help="Ação a ser executada. Use '<comando> -h' para mais ajuda sobre um comando específico."
+)
+
+# Subparser para ajustar PCA
+parser_pca = subparsers.add_parser("fit_pca", help="Ajusta e salva PCA.")
+
+# Subparser para ajustar modelo AR
+parser_ar = subparsers.add_parser("fit_ar", help="Ajusta o modelo AR inicial.")
+parser_ar.add_argument("--num_ar_iters", type=int, default=50, help="Número de iterações para o ajuste do modelo AR. Este também será o número da iteração do checkpoint.")
+
+# Subparser para ajustar modelo AR-HMM
+parser_arhmm = subparsers.add_parser("fit_arhmm", help="Ajusta o modelo AR-HMM, carregando um modelo AR pré-treinado.")
+parser_arhmm.add_argument("--num_ar_iters_checkpoint", type=int, required=True, help="Número da iteração do checkpoint do modelo AR a ser carregado (ex: 50 se o modelo AR foi treinado por 50 iterações).")
+parser_arhmm.add_argument("--total_iters", type=int, default=500, help="Número total de iterações para o ajuste do modelo AR-HMM.")
+parser_arhmm.add_argument("--kappa", type=float, default=None, help="Valor de Kappa para o modelo AR-HMM. Sobrescreve o valor no config.yml se fornecido.")
+
+# Subparser para varredura de kappa
+parser_kappa = subparsers.add_parser("kappa_scan", help="Realiza uma varredura de kappa: ajusta AR-HMM para múltiplos valores de kappa.")
+parser_kappa.add_argument("--num_ar_iters_checkpoint", type=int, required=True, help="Número da iteração do checkpoint do modelo AR a ser carregado para cada varredura.")
+parser_kappa.add_argument("--base_total_iters", type=int, default=200, help="Número total de iterações para cada modelo AR-HMM na varredura.")
+parser_kappa.add_argument("--kappa_values", type=str, required=True, help="Lista de valores de kappa separados por vírgula para varredura (ex: '1e3,1e4,1e5').")
+
+
+args = parser.parse_args()
+
+for key, value in vars(args).items():
+    globals()[key] = value
