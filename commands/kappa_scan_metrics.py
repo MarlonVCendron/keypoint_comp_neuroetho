7e2ef520d86afb863b4c333f739f781089a62710
Marlon <marlonvcendron@gmail.com>
2025-10-05 08:27:14 -0300

    scan

--- commands/kappa_scan_metrics.py
+++ commands/kappa_scan_metrics.py
@@ -4,13 +4,11 @@ import numpy as np
 
 def kappa_scan_metrics(project_dir):
     model_names = [
-        # "kappa_scan_3.0_7.0_5_10-1000.0",
-        # "kappa_scan_3.0_7.0_5_10-10000.0",
-        # "kappa_scan_3.0_7.0_5_10-100000.0",
-        # "kappa_scan_3.0_7.0_5_10-1000000.0",
-        # "kappa_scan_3.0_7.0_5_10-10000000.0",
-        "kappa_scan_4.0_6.0_3_10-10000.0",
-        "kappa_scan_5.0_6.0_2_10-100000.0",
+        "kappa_scan_3.6_5.0_5_10-3981.0717055349733",
+        "kappa_scan_3.6_5.0_5_10-8912.509381337459",
+        "kappa_scan_3.6_5.0_5_10-19952.62314968879",
+        "kappa_scan_3.6_5.0_5_10-44668.359215096345",
+        "kappa_scan_3.6_5.0_5_10-100000.0"
     ]
 
     eml_scores, eml_std_errs = kpms.expected_marginal_likelihoods(project_dir, model_names)
