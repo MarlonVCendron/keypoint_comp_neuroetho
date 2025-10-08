d7848e98800ab2ea01763a945bf0f00003dbbbfe
Marlon <marlonvcendron@gmail.com>
2025-10-08 10:57:41 -0300

    eml plot

--- commands/kappa_scan_metrics.py
+++ commands/kappa_scan_metrics.py
@@ -4,15 +4,13 @@ import numpy as np
 
 def kappa_scan_metrics(project_dir):
     model_names = [
-        "kappa_scan_3.6_5.0_5_10-3981.0717055349733",
-        "kappa_scan_3.6_5.0_5_10-8912.509381337459",
-        "kappa_scan_3.6_5.0_5_10-19952.62314968879",
-        "kappa_scan_3.6_5.0_5_10-44668.359215096345",
-        "kappa_scan_3.6_5.0_5_10-100000.0"
+        '12k_no_tail',
+        '9k'
     ]
 
     eml_scores, eml_std_errs = kpms.expected_marginal_likelihoods(project_dir, model_names)
     best_model = model_names[np.argmax(eml_scores)]
     print(f"Best model: {best_model}")
 
-    kpms.plot_eml_scores(eml_scores, eml_std_errs, model_names)
+    fig, ax = kpms.plot_eml_scores(eml_scores, eml_std_errs, model_names)
+    fig.savefig(f"{project_dir}/eml_scores.png")
